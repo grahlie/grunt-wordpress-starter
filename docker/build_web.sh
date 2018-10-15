82a7389642fe6d8010d6011827241abbc4686cbe
grahlie <mathias.grahl@fast2.se>
2018-10-15 22:07:48 +0200

    moved to wordpress docker image instead

--- docker/build_web.sh
+++ docker/build_web.sh
@@ -5,6 +5,7 @@ UNAME=`uname`
 which -s jq >/dev/null
 if [[ $? == 1 ]]; then 
     if [[ "$UNAME" == 'Linux' ]]; then
+        # TODO: this doesn't work
         sudo apt-get --assume-yes install jq
     elif [[ "$UNAME" == 'Darwin' ]]; then
         brew install jq
@@ -12,7 +13,18 @@ if [[ $? == 1 ]]; then
 fi
 
 echo "2) Set some variables"
-FILE="../config.json"
+# Check if we running in production
+if [[ $inputVariable == 'production' ]]; then
+    FILE="../config.prod.json"
+    if [[ ! -d $FILE ]]; then
+        echo "config.prod.json Doesn't exists you need to create this first!"
+        echo "exiting script here"
+        exit;
+    fi
+else
+    FILE="../config.json"
+fi
+
 NAME=$(jq .name $FILE)
 FOLDER=$(jq .grunt.deploy $FILE)
 PORT=$(jq .docker.port $FILE)
@@ -24,6 +36,7 @@ DBVOLUME=$(jq .docker.dbvolume $FILE)
 DEBUG=$(jq .docker.debug $FILE)
 MACHINE=$(jq .docker.machine $FILE)
 MULTISITE=$(jq .docker.multisite $FILE)
+VERSION=$(jq .docker.version $FILE)
 
 echo "3) Strip specialchars from JSON value"
 # TODO: Find a solution to loop this
@@ -49,10 +62,12 @@ MACHINE="${MACHINE%\"}"
 MACHINE="${MACHINE#\"}"
 MULTISITE="${MULTISITE%\"}"
 MULTISITE="${MULTISITE#\"}"
+VERSION="${VERSION%\"}"
+VERSION="${VERSION#\"}"
 
 echo "4) Create compose file"
 dockercompose="./docker-compose.yml"
-if ! [ -e $dockercompose ]; then
+if ! [[ -e $dockercompose ]]; then
     echo "version: '2'"                                                     >> ./docker-compose.yml
     echo "services:"                                                        >> ./docker-compose.yml
     echo "  "$NAME"-db:"                                                    >> ./docker-compose.yml
@@ -67,23 +82,24 @@ if ! [ -e $dockercompose ]; then
     echo "      - ./database.sql:/docker-entrypoint-initdb.d/database.sql"  >> ./docker-compose.yml
     echo "    container_name: " $NAME"-db"                                  >> ./docker-compose.yml
     echo "  "$NAME":"                                                       >> ./docker-compose.yml
-    echo "    build: ."                                                     >> ./docker-compose.yml
+    echo "    image: wordpress:"$VERSION                                    >> ./docker-compose.yml
     echo "    volumes:"                                                     >> ./docker-compose.yml
-    echo "      - "$FOLDER"/:/var/www/"                                     >> ./docker-compose.yml
+    echo "      - "$FOLDER":/var/www/html/wp-content/themes/"$NAME          >> ./docker-compose.yml
     echo "    expose:"                                                      >> ./docker-compose.yml
     echo "      - 80"                                                       >> ./docker-compose.yml
     echo "    depends_on:"                                                  >> ./docker-compose.yml
     echo "      - "$NAME"-db"                                               >> ./docker-compose.yml
     echo "    restart: always"                                              >> ./docker-compose.yml
     echo "    environment:"                                                 >> ./docker-compose.yml
-    echo "      VIRTUAL_HOST:" $DOMAIN", wwww."$DOMAIN                      >> ./docker-compose.yml
-    echo "      DBNAME:" $DBNAME                                            >> ./docker-compose.yml
-    echo "      DBUSER:" $DBUSER                                            >> ./docker-compose.yml
-    echo "      DBPASS:" $DBPASS                                            >> ./docker-compose.yml
-    echo "      DBHOST:" $NAME"-db"                                         >> ./docker-compose.yml
+    echo "      VIRTUAL_HOST:" $DOMAIN", www."$DOMAIN                       >> ./docker-compose.yml
+    echo "      WORDPRESS_DB_NAME:" $DBNAME                                 >> ./docker-compose.yml
+    echo "      WORDPRESS_DB_USER:" $DBUSER                                 >> ./docker-compose.yml
+    echo "      WORDPRESS_DB_PASSWORD:" $DBPASS                             >> ./docker-compose.yml
+    echo "      WORDPRESS_DB_HOST:" $NAME"-db"                              >> ./docker-compose.yml
+    if [[ $DEBUG == 1 ]]; then 
+        echo "      WORDPRESS_DEBUG:" $DEBUG                                >> ./docker-compose.yml
+    fi
     echo "      DOMAIN:" $DOMAIN                                            >> ./docker-compose.yml
-    echo "      DEBUG:" $DEBUG                                              >> ./docker-compose.yml
-    echo "      MULTISITE:" $MULTISITE                                      >> ./docker-compose.yml
     echo "    container_name: " $NAME                                       >> ./docker-compose.yml
     echo "  nginx-proxy:"                                                   >> ./docker-compose.yml
     echo "    image: jwilder/nginx-proxy"                                   >> ./docker-compose.yml
@@ -98,17 +114,10 @@ if ! [ -e $dockercompose ]; then
     echo "      name: nginx-proxy"                                          >> ./docker-compose.yml
 fi
 
-
-echo "5) Compose build"
-docker-compose build
-
-echo "6) Compose down (fix for old cache)"
-docker-compose -f docker-compose.yml down
-
-echo "7) Compose up"
+echo "5) Compose up"
 docker-compose up -d
 
-echo "8) Configure hosts file"
+echo "6) Configure hosts file"
 HOSTIP="$(docker-machine ip $MACHINE)"
 if grep -Fxq "$HOSTIP $DOMAIN" /etc/hosts; then
     echo "Already exists in host file"
@@ -117,3 +126,7 @@ else
     sudo cp /etc/hosts /etc/hosts.backup
     sudo -- sh -c "echo $HOSTIP $DOMAIN >> /etc/hosts"
 fi
+
+echo "7) You're up and running buddy!"
+echo "The website is running on http://$DOMAIN"
+echo "Next command you can run is grunt watch to start develop your theme"
--- docker/wordpress.sh
+++ docker/wordpress.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 
-echo "1) Create htaccess"
-if [ "$MULTISITE" -gt 0 ]; then
-    echo "# BEGIN WordPress"                                                            >>/var/www/.htaccess
-    echo "<IfModule mod_rewrite.c>"                                                     >>/var/www/.htaccess
-    echo "RewriteEngine On"                                                             >>/var/www/.htaccess
-    echo "RewriteBase /"                                                                >>/var/www/.htaccess
-    echo "RewriteRule ^server-status - [L]"                                             >>/var/www/.htaccess
-    echo "RewriteRule ^index\.php$ - [L]"                                               >>/var/www/.htaccess
-    echo "# add a trailing slash to /wp-admin"                                          >>/var/www/.htaccess
-    echo "RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]"               >>/var/www/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} -f [OR]"                                      >>/var/www/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} -d"                                           >>/var/www/.htaccess
-    echo "RewriteRule ^ - [L]"                                                          >>/var/www/.htaccess
-    echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]"        >>/var/www/.htaccess
-    echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]"                             >>/var/www/.htaccess
-    echo "RewriteRule . index.php [L]"                                                  >>/var/www/.htaccess
-    echo "</IfModule>"                                                                  >>/var/www/.htaccess
-    echo "# END WordPress"                                                              >>/var/www/.htaccess
-else
-    echo "# BEGIN WordPress"                                                            >>/var/www/.htaccess
-    echo "<IfModule mod_rewrite.c>"                                                     >>/var/www/.htaccess
-    echo "RewriteEngine On"                                                             >>/var/www/.htaccess
-    echo "RewriteBase /"                                                                >>/var/www/.htaccess
-    echo "RewriteRule ^server-status - [L]"                                             >>/var/www/.htaccess
-    echo "RewriteRule ^index\.php$ - [L]"                                               >>/var/www/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} !-f"                                          >>/var/www/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} !-d"                                          >>/var/www/.htaccess
-    echo "RewriteRule . /index.php [L]"                                                 >>/var/www/.htaccess
-    echo "</IfModule>"                                                                  >>/var/www/.htaccess
-    echo "# END WordPress"                                                              >>/var/www/.htaccess
-fi
+# echo "1) Create htaccess"
+# if [ "$MULTISITE" -gt 0 ]; then
+#     echo "# BEGIN WordPress"                                                            >>/var/www/.htaccess
+#     echo "<IfModule mod_rewrite.c>"                                                     >>/var/www/.htaccess
+#     echo "RewriteEngine On"                                                             >>/var/www/.htaccess
+#     echo "RewriteBase /"                                                                >>/var/www/.htaccess
+#     echo "RewriteRule ^server-status - [L]"                                             >>/var/www/.htaccess
+#     echo "RewriteRule ^index\.php$ - [L]"                                               >>/var/www/.htaccess
+#     echo "# add a trailing slash to /wp-admin"                                          >>/var/www/.htaccess
+#     echo "RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]"               >>/var/www/.htaccess
+#     echo "RewriteCond %{REQUEST_FILENAME} -f [OR]"                                      >>/var/www/.htaccess
+#     echo "RewriteCond %{REQUEST_FILENAME} -d"                                           >>/var/www/.htaccess
+#     echo "RewriteRule ^ - [L]"                                                          >>/var/www/.htaccess
+#     echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]"        >>/var/www/.htaccess
+#     echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]"                             >>/var/www/.htaccess
+#     echo "RewriteRule . index.php [L]"                                                  >>/var/www/.htaccess
+#     echo "</IfModule>"                                                                  >>/var/www/.htaccess
+#     echo "# END WordPress"                                                              >>/var/www/.htaccess
+# else
+#     echo "# BEGIN WordPress"                                                            >>/var/www/.htaccess
+#     echo "<IfModule mod_rewrite.c>"                                                     >>/var/www/.htaccess
+#     echo "RewriteEngine On"                                                             >>/var/www/.htaccess
+#     echo "RewriteBase /"                                                                >>/var/www/.htaccess
+#     echo "RewriteRule ^server-status - [L]"                                             >>/var/www/.htaccess
+#     echo "RewriteRule ^index\.php$ - [L]"                                               >>/var/www/.htaccess
+#     echo "RewriteCond %{REQUEST_FILENAME} !-f"                                          >>/var/www/.htaccess
+#     echo "RewriteCond %{REQUEST_FILENAME} !-d"                                          >>/var/www/.htaccess
+#     echo "RewriteRule . /index.php [L]"                                                 >>/var/www/.htaccess
+#     echo "</IfModule>"                                                                  >>/var/www/.htaccess
+#     echo "# END WordPress"                                                              >>/var/www/.htaccess
+# fi
 
 echo "2) Create wp-config"
 wpconfig="/var/www/wp-config.php"
@@ -73,8 +73,14 @@ curl -s https://api.wordpress.org/secret-key/1.1/salt/ >>/var/www/wp-config.php
 
 echo ""                                                >>/var/www/wp-config.php
 
+# Debug mode
+if [ "$DEBUG" -gt 0 ]; then
+    echo "define('WP_DEBUG', true);"                  >>/var/www/wp-config.php
+else
+    echo "define('WP_DEBUG', false);"                 >>/var/www/wp-config.php
+fi
+
 # Other settings
-echo "define('WP_DEBUG', false);"                      >>/var/www/wp-config.php
 echo "define('WP_AUTO_UPDATE_CORE', false);"           >>/var/www/wp-config.php
 echo "define('WP_MEMORY_LIMIT', '96M');"               >>/var/www/wp-config.php
 echo "define( 'WP_POST_REVISIONS', 100 );"             >>/var/www/wp-config.php
